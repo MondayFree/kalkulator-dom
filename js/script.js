30e27ed91f95ead9b0d3d9adf2803672c2e6b5be
luji <luzirmdhn@gmail.com>
2023-05-07 22:56:03 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ const board = document.querySelector(".result");
 operator.forEach(el => {
   el.addEventListener('click', event => {
     let result;
-    const val1 = input1.value;
-    const val2 = input2.value;
-    if(val1.length === 0 && val2.length === 0) return;
+    const val1 = parseInt(input1.value);
+    const val2 = parseInt(input2.value);
+    if(!val1 || !val2) return;
     switch(event.target.value) {
       case "+" :
         result = val1 + val2;
